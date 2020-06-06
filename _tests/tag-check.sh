efecb48c87df273a21f6cbe00596d665818777f7
Nick Janetakis <nick.janetakis@gmail.com>
2020-06-06 15:19:33 -0400

    Add GitHub Actions
    With so many tag files it's really easy for things to get out of sync or
    to forget things. Perhaps additional tests will be created later too.

---
+++ _tests/tag-check.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Ensure every referenced tag in an episode or interview exists as a tag file.
+diff --color -u <(for file in $(find _posts)
+do
+    [[ -f "${file}" ]] && sed -n "/^tags:$/,/^title:/p" "${file}" | grep "\S" \
+        | tail -n +2 | head -n -1 | cut -d '"' -f 2
+done | sort -u) <(cd tags && ls *.md | cut -d "." -f 1 | sort -u)
+
+# Ensure every tag has a layout, url, title and description.
+FILE_TAG_COUNT=$(grep "tag:" -R tags/*.md | wc -l)
+FILE_LAYOUT_COUNT=$(grep "layout:" -R tags/*.md | wc -l)
+FILE_URL_COUNT=$(grep "url:" -R tags/*.md | wc -l)
+FILE_TITLE_COUNT=$(grep "title:" -R tags/*.md | wc -l)
+FILE_DESCRIPTION_COUNT=$(grep "description" -R tags/*.md | wc -l)
+
+[[ "${FILE_TAG_COUNT}" = "${FILE_LAYOUT_COUNT}" \
+    && "${FILE_TAG_COUNT}" = "${FILE_URL_COUNT}" \
+    && "${FILE_TAG_COUNT}" = "${FILE_TITLE_COUNT}" \
+    && "${FILE_TAG_COUNT}" = "${FILE_DESCRIPTION_COUNT}" ]]
