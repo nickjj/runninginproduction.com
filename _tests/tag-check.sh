e79860ce3ff188a891820434575ae0be65678b6e
Nick Janetakis <nick.janetakis@gmail.com>
2020-06-13 08:02:40 -0400

    Refactor tag-check script
    Since I want to make a video around this script, I figured it would be a
    good idea to spend a few minutes to think of ways to improve it.
    
    I like the strategy of make it work, make it nice and then make it fast.

--- _tests/tag-check.sh
+++ _tests/tag-check.sh
@@ -3,11 +3,10 @@
 set -e
 
 # Ensure every referenced tag in an episode or interview exists as a tag file.
-diff --color -u <(for file in $(find _posts)
+diff --color -u <(for file in $(find _posts -type f)
 do
-    [[ -f "${file}" ]] && sed -n "/^tags:$/,/^title:/p" "${file}" | grep "\S" \
-        | tail -n +2 | head -n -1 | cut -d '"' -f 2
-done | sort -u) <(cd tags && ls *.md | cut -d "." -f 1 | sort -u)
+    sed -n "/^tags:$/,/^title:/p" "${file}" | grep "^  - " | cut -d '"' -f 2
+done | sort -u) <(basename -s .md tags/*.md | sort -u)
 
 # Ensure every tag has a layout, url, title and description.
 FILE_TAG_COUNT="$(grep "^tag: \".*\"$" -R tags/*.md | wc -l)"
